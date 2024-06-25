43e76eabbcb517fc6e36246e3e6cba90b0a408ce
Zhu-Peter <gzhu@ualberta.ca>
2024-06-24 19:59:46 -0600

    added bonus queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
+-- INITIALIZE TABLES --
 CREATE TABLE blogger (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) UNIQUE NOT NULL,
@@ -27,18 +28,23 @@ CREATE TABLE blog_post_comment (
 );
 
 INSERT INTO blogger (username, password, name, created_at, email) VALUES
-('JaneDoe', 'securePass123', 'Jane Doe', '2024-06-24 10:00:00', 'jane.doe@email.com'),
-('JohnSmith', 'password123', 'John Smith', '2024-06-23 09:30:00', 'john.smith@email.com');
+('JaneDoe', 'securePass123', 'Jane Doe', '2020-01-24 10:00:00', 'jane.doe@email.com'),
+('JohnSmith', 'password123', 'John Smith', '2020-02-23 09:30:00', 'john.smith@email.com')
+('Daniel', 'pass', 'Daniel Johnson', '2021-03-24 10:00:00', 'daniel@email.com'),
+('Matt', 'pass', 'Matt Davids', '2022-04-24 10:00:00', 'matt@email.com');
 
 INSERT INTO blog_post (content, title, created_at, blogger_id) VALUES
-('This is the content of the first blog post', 'First Post', '2020-05-25 11:00:00', 1),
-('This is the content of the second blog post', 'Second Post', '2024-06-24 12:00:00', 1);
-('This is the content of the first blog post', 'First Post', '2020-05-25 11:00:00', 2),
-('This is the content of the second blog post', 'Second Post', '2024-06-24 12:00:00', 2);
+('This is the content of the first blog post - Jane', 'First Post', '2020-05-25 11:00:00', 1),
+('This is the content of the second blog post - Jane', 'Second Post', '2024-06-24 12:00:00', 1),;
+('This is the content of the first blog post - John', 'First Post', '2020-05-25 11:00:00', 2),
+('This is the content of the second blog post - John', 'Second Post', '2024-06-24 12:00:00', 2);
 
 INSERT INTO blog_post_comment (content, created_at, blogger_id, blog_post_id) VALUES
 ('Great post!', '2024-06-24 11:30:00', 2, 1),
-('Thank you for sharing this.', '2024-06-24 12:30:00', 1, 2);
+('Thank you for sharing this.', '2024-06-24 12:30:00', 1, 2),
+('Great post x 2!', '2024-06-24 11:30:00', 3, 1),
+('Great post! x 3', '2024-06-24 11:30:00', 4, 1),
+('Great ok delete me', '2024-06-24 11:30:00', 1, 1);
 
 -- Queries --
 
@@ -58,3 +64,16 @@ INNER JOIN blog_post
 ON blogger.id = blog_post.blogger_id
 INNER JOIN blog_post_comment 
 ON blog_post.id = blog_post_comment.blog_post_id;
+
+-- BONUS --
+
+DELETE FROM blog_post_comment
+WHERE blog_post_comment.content LIKE '%delete me%';
+
+SELECT blog_post_comment.content, title, username FROM blogger
+INNER JOIN blog_post 
+ON blogger.id = blog_post.blogger_id
+INNER JOIN blog_post_comment 
+ON blog_post.id = blog_post_comment.blog_post_id
+GROUP BY blog_post_id
+HAVING COUNT(blog_post_id) >= 3;
